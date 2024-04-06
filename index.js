187db7f49c4546b9e3fbd6da7d63b8062c2b30d5
Sylvia <sylvia.tata@student.moringaschool.com>
2024-04-06 13:06:43 +0300

    oject operations

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your solution in this file!
+const employee = {
+    name: "Sylvia Tata",
+    streetAddress: "00604 Kabete Rd"
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+        ...employee,
+        [key]: value
+    };
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
+
+console.log("Original Employee:", employee);
+
+const updatedEmployee = updateEmployeeWithKeyAndValue(employee, "city", "Nairobi");
+console.log("Updated Employee (Non-destructive):", updatedEmployee);
+
+destructivelyUpdateEmployeeWithKeyAndValue(employee, "city", "Nairobi");
+console.log("Updated Employee (Destructive):", employee);
+
+const deletedEmployee = deleteFromEmployeeByKey(employee, "streetAddress");
+console.log("Employee after deletion (Non-destructive):", deletedEmployee);
+
+destructivelyDeleteFromEmployeeByKey(employee, "streetAddress");
+console.log("Employee after deletion (Destructive):", employee);
+
